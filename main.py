7cf50ea27c9025c1d7fe7f8918c4f56d97a93b49
Preetam Ahuja <preetamahuja9211@gmail.com>
2022-07-11 15:52:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Command(BaseModel):
+    command: str
 
 app = FastAPI()
 
@@ -8,17 +12,6 @@ def read_root():
     return {"200": "Welcome To Heroku"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.get("/fizz_buzz/{num}")
-def read_item(num: int):
-    # https: // ja.wikipedia.org / wiki / Fizz_Buzz
-    if not num % 15:
-        return {num: "Fizz Buzz"}
-    elif not num % 5 or not num % 3:
-        return {num: 'Fizz' if not num % 3 else 'Buzz'}
-    else:
-        return {num: 'Stay Silent'}
+@app.post("/command/{session_id}")
+async def read_item(item_id: int, command: Command | None):
+    return {"item_id": item_id, "command": command}
