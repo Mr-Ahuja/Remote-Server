538971fa6055a8bc466dde142a483327de9c6eb6
ahujp1 <preetam.ahuja@in.pega.com>
2022-11-08 18:11:22 +0530

    Working

--- upload.py
+++ upload.py
@@ -3,6 +3,6 @@ import sys
 
 server = "https://remote-terminal-mrahuja.herokuapp.com"
 url = server + '/upload'
-file = {'file': open(sys.argv[0], 'rb')}
+file = {'file': open(sys.argv[1], 'rb')}
 resp = requests.post(url=url, files=file) 
 print(resp.json())
